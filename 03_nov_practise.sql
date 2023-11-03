31aa53f4e9232f4a80bda35b0545eb2323124ac0
Santiago Bülent Karadeniz <105861484+BulentKaradeniz@users.noreply.github.com>
2023-11-03 18:25:35 +0300

    03

--- 03_nov_practise.sql
+++ 03_nov_practise.sql
@@ -14,3 +14,43 @@ rename column   soyisim to ilce_adı;
 
 alter table sehirler1
 rename column isim to  il_adi;
+
+-------------------------------------
+
+create table calisanlar2
+(
+	id char(5) primary key,
+	isim varchar(10) unique,
+	maas real not null,
+	 ise_baslama date
+);
+
+INSERT INTO calisanlar VALUES('10002', 'Mehmet Yılmaz' ,12000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10008', null, 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10010', Mehmet Yılmaz, 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10004', 'Veli Han', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10005', 'Mustafa Ali', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10006', 'Canan Yaş', NULL, '2019-04-12');
+INSERT INTO calisanlar VALUES('10003', 'CAN', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10007', 'CAN', 5000, '2018-04-14');
+INSERT INTO calisanlar VALUES('10009', 'cem', '', '2018-04-14');
+INSERT INTO calisanlar VALUES('', 'osman', 2000, '2018-04-14');
+INSERT INTO calisanlar VALUES('', 'osman can', 2000, '2018-04-14');
+INSERT INTO calisanlar VALUES( '10002', 'ayse Yılmaz' ,12000, '2018-04-14');
+INSERT INTO calisanlar VALUES( null, 'filiz ' ,12000, '2018-04-14');
+
+select * from calisanlar2;
+
+create table  adresler 
+(
+	adres_id char(10),
+	sokak varchar(50),
+	cadde varchar(50),
+	sehir varchar(20),
+	constraint adresler_fk foreign key (adres_id) references calisanlar2(id)
+
+);
+
+select * from  adresler;
+
+
